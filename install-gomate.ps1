e6980ac852e1778f9835f25d1fd957986b7955bc
WiseScripts <122132746+WiseScripts@users.noreply.github.com>
2025-11-08 19:12:55 +0800

    Update install-gomate.ps1

--- install-gomate.ps1
+++ install-gomate.ps1
@@ -15,7 +15,7 @@ param(
 )
 
 # --- 配置信息 (请根据您的仓库信息修改) ---
-$GitHubUser = "WiseScripts"  # <-- 替换为您的 GitHub 用户名
+$GitHubUser = "WiseScripts" # <-- 替换为您的 GitHub 用户名
 $GitHubRepo = "gomate" # <-- 替换为您的仓库名
 $AssetName = "gomate.exe"
 
